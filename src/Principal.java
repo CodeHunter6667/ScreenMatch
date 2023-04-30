1b154a9bd055327b902355130dc5be06f659e4ab
CodeHunter6667 <124934157+CodeHunter6667@users.noreply.github.com>
2023-04-30 18:36:26 -0300

    Atribuição de variáveis no Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,9 @@
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        
+
+        meuFilme.nome = "Top Gun:MAverick";
+        meuFilme.anoDeLancamento = 2022;
+
     }
 }
