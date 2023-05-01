89fbb246637d2f0b47f9c49b74597e1d8e84d8c6
CodeHunter6667 <124934157+CodeHunter6667@users.noreply.github.com>
2023-05-01 14:58:14 -0300

    Implementação dos principios da Orientação a objeto e de interfaces

--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,21 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
 
-        meuFilme.nome = "Top Gun:Maverick";
-        meuFilme.anoDeLancamento = 2022;
+        meuFilme.setNome("Top Gun:Maverick");
+        meuFilme.setAnoDeLancamento(2022);
+        meuFilme.setDuracaoEmMinutos(131);
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar - O Caminho da Agua");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(190);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -15,5 +25,29 @@ public class Principal {
         System.out.println(meuFilme.getSomaDasAvaliacoes());
         System.out.println(meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
+
+        Serie serie = new Serie();
+
+        serie.setNome("Lost");
+        serie.setTemporadas(10);
+        serie.setEpisodiosPorTemporada(10);
+        serie.setMinutosPorEpisodio(50);
+        serie.setAnoDeLancamento(2000);
+        serie.exibeFichaTecnica();
+        System.out.println("Duração para maratonar Lost: " + serie.getDuracaoEmMinutos());
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(serie);
+        System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+        Episodio episodio = new Episodio();
+        episodio.setSerie(serie);
+        episodio.setNumero(1);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
     }
 }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,40 +1,20 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+import br.com.alura.screenmatch.calculos.Classificavel;
 
-    public double getSomaDasAvaliacoes() {
-        return this.somaDasAvaliacoes;
+public class Filme extends Titulo implements Classificavel {
+    private String diretor;
+
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void setSomaDasAvaliacoes(double somaDasAvaliacoes) {
-        this.somaDasAvaliacoes = somaDasAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 
-    public int getTotalDeAvaliacoes() {
-        return this.totalDeAvaliacoes;
-    }
-
-    public void setTotalDeAvaliacoes(int totalDeAvaliacoes) {
-        this.totalDeAvaliacoes = totalDeAvaliacoes;
-    }
-
-    public void exibeFichaTecnica(){
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-    }
-
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public double pegaMedia(){
-        return somaDasAvaliacoes / totalDeAvaliacoes;
+    @Override
+    public int getClassificacao() {
+        return (int) pegaMedia() / 2;
     }
 }
